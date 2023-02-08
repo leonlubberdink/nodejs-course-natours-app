7d17b93ad66404c173001747ef77970a7b48dbfb
leonlubberdink <mail@leonlubberdink.com>
2023-02-08 17:11:42 +0100

    Lecture 127 - Managing passwords (Hash wih bcryptjs)

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      //This only works on User.create and User.save!!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords don't match",
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  //Only run if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //hash password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
