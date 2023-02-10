b8e4e807e1a34e8b00ddbf7dfeead94977787248
leonlubberdink <mail@leonlubberdink.com>
2023-02-10 10:06:37 +0100

    Lecture 140 - Implemented option for user to delete their account (set active: false)

--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -68,6 +73,12 @@ userSchema.methods.correctPassword = async function (
 ) {
   return await bcrypt.compare(inputPassword, userPassword);
 };
+
+userSchema.pre(/^find/, function (next) {
+  // "this" points to the current query
+  this.find({ active: { $ne: false } });
+  next();
+});
 
 userSchema.methods.changedPasswordAfter = function (JWTTimeStamp) {
   if (this.passwordChangedAt) {
