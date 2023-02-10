e287570abf2773b29f2405967f86d2a75ad42d54
leonlubberdink <mail@leonlubberdink.com>
2023-02-10 15:06:07 +0100

    Lecture 143-144 - Implemented Rate limiting and security headers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/AppError');
 const errorController = require('./controllers/errorController');
@@ -9,14 +11,31 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 ////////////////////////////////
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+
+//Set Security HTTP headers
+app.use(helmet());
+
+// Dev Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+//body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   console.log(req.headers);
 
